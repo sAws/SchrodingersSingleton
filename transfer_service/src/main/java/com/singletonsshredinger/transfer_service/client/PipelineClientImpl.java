b71964892fc01d342fbf45bb72f4b64a398635c4
Sergey Turguzov <s.turguzov@gmail.com>
2021-06-06 10:55:34 +0300

    Added ui

--- transfer_service/src/main/java/com/singletonsshredinger/transfer_service/client/PipelineClientImpl.java
+++ transfer_service/src/main/java/com/singletonsshredinger/transfer_service/client/PipelineClientImpl.java
@@ -5,12 +5,12 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.singletonsshredinger.transfer_service.exceptions.AppException;
 import com.singletonsshredinger.transfer_service.model.Pipeline;
-//import org.apache.http.HttpEntity;
-//import org.apache.http.client.methods.CloseableHttpResponse;
-//import org.apache.http.client.methods.HttpGet;
-//import org.apache.http.impl.client.CloseableHttpClient;
-//import org.apache.http.impl.client.HttpClients;
-//import org.apache.http.util.EntityUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.io.IOException;
@@ -27,32 +27,32 @@ public class PipelineClientImpl implements PipelineClient {
     public List<Pipeline> getAllPipeLines() throws AppException {
         List<Pipeline> pipelines = new ArrayList<>();
 
-//        CloseableHttpClient httpclient = HttpClients.createDefault();
-//        HttpGet httpGet = new HttpGet(URL + "/api/pipelines");
-//        HttpEntity entity;
-//        CloseableHttpResponse response;
-//        try {
-//            response = httpclient.execute(httpGet);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            throw new AppException("error execution query");
-//        }
-//
-//        try {
-//            entity = response.getEntity();
-//            String result = EntityUtils.toString(entity);
-//            Type listType = new TypeToken<ArrayList<Pipeline>>(){}.getType();
-//            pipelines = new Gson().fromJson(result, listType);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            throw new AppException("error parsing entity");
-//        } finally {
-//            try {
-//                response.close();
-//            } catch (IOException ignored) {
-//
-//            }
-//        }
+       CloseableHttpClient httpclient = HttpClients.createDefault();
+       HttpGet httpGet = new HttpGet(URL + "/api/pipelines");
+       HttpEntity entity;
+       CloseableHttpResponse response;
+       try {
+           response = httpclient.execute(httpGet);
+       } catch (IOException e) {
+           e.printStackTrace();
+           throw new AppException("error execution query");
+       }
+
+       try {
+           entity = response.getEntity();
+           String result = EntityUtils.toString(entity);
+           Type listType = new TypeToken<ArrayList<Pipeline>>(){}.getType();
+           pipelines = new Gson().fromJson(result, listType);
+       } catch (IOException e) {
+           e.printStackTrace();
+           throw new AppException("error parsing entity");
+       } finally {
+           try {
+               response.close();
+           } catch (IOException ignored) {
+
+           }
+       }
         return pipelines;
     }
 }
